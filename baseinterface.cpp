431ba02ba3d950c226da41ff5a93719631a94ab1
Alex <alexandroff.m@gmail.com>
2015-10-17 20:21:16 +0600

    Добавлен минимальный набор  юнит-тестов

--- baseinterface.cpp
+++ baseinterface.cpp
@@ -11,28 +11,7 @@
  */
 bool BaseInterface::read_from_file(string file_name)
 {
-    ifstream in_file(file_name.c_str());
-    if(!in_file.good()) return false;
-
-    in_file >> size;
-    int tmp;
-    ig.push_back(0);
-    int count = 0;
-    for(size_t i = 0; i < size; i++) {
-        for(size_t j = 0; j < size; j++) {
-            in_file >> tmp;
-            if(tmp != 0) {
-                gg.push_back(tmp);
-                jg.push_back(j);
-                count ++;
-            }
-        }
-        ig.push_back(count);
-    }
-    in_file.close();
-    if(count == 0)
-        return false;
-    return true;
+    return read_from_file_service(file_name, false);
 }
 
 /**
@@ -67,4 +46,112 @@ bool BaseInterface::write_to_file(string file_name)
     return true;
 }
 
+bool BaseInterface::read_from_file_service(string file_name, bool is_unit_test)
+{
+    ifstream in_file(file_name.c_str());
+    if(!in_file.good()) return false;
 
+    size_t curr_size;
+    if(is_unit_test) {
+        in_file >> size_answer;
+        curr_size = size_answer;
+    }
+    else {
+        in_file >> size;
+        curr_size = size;
+    }
+    int tmp;
+    if(is_unit_test)
+        ig_answer.push_back(0);
+    else
+        ig.push_back(0);
+    int count = 0;
+    for(size_t i = 0; i < curr_size; i++) {
+        for(size_t j = 0; j < curr_size; j++) {
+            in_file >> tmp;
+            if(tmp != 0) {
+                if(is_unit_test) {
+                    gg_answer.push_back(tmp);
+                    jg_answer.push_back(j);
+                }
+                else {
+                    gg.push_back(tmp);
+                    jg.push_back(j);
+                }
+                count ++;
+            }
+        }
+        if(is_unit_test)
+            ig_answer.push_back(count);
+        else
+            ig.push_back(count);
+    }
+    in_file.close();
+    if(count == 0)
+        return false;
+    return true;
+}
+
+size_t BaseInterface::run_tests()
+{
+    if(!test1() > 0)
+        cout << "1\t[OK]" << endl;
+    else
+        cout << "1\t[FAIL]" << endl;
+
+    if(!test2() > 0)
+        cout << "2\t[OK]" << endl;
+    else
+        cout << "2\t[FAIL]" << endl;
+
+    if(test3() > 0)
+        cout << "3\t[OK]" << endl;
+    else
+        cout << "3\t[FAIL]" << endl;
+    return 0;
+}
+
+bool BaseInterface::read_answer_from_file(string file_name)
+{
+    return read_from_file_service(file_name, true);
+}
+
+size_t BaseInterface::compare()
+{
+    if(size != size_answer) return 1;
+    if(ig.size() != ig_answer.size()) return 2;
+    if(gg.size() != gg_answer.size()) return 3;
+    if(jg.size() != jg_answer.size()) return 4;
+    for(size_t i = 0; i < ig.size(); i++)
+        if(ig[i] != ig_answer[i]) return 5;
+    for(size_t i = 0; i < gg.size(); i++)
+        if(gg[i] != gg_answer[i]) return 6;
+    for(size_t i = 0; i < ig.size(); i++)
+        if(jg[i] != jg_answer[i]) return 7;
+
+    return 0;
+}
+
+size_t BaseInterface::test1()
+{
+    read_from_file("UnitTests/test1.txt");
+    solve();
+    read_answer_from_file("UnitTests/test1_answer.txt");
+    return compare();
+}
+
+size_t BaseInterface::test2()
+{
+    read_from_file("UnitTests/test2.txt");
+    solve();
+    read_answer_from_file("UnitTests/test2_answer.txt");
+    return compare();
+}
+
+size_t BaseInterface::test3()
+{
+    read_from_file("UnitTests/test3.txt");
+    solve();
+    read_answer_from_file("UnitTests/test3_answer.txt");
+    return compare();
+}
--- baseinterface.h
+++ baseinterface.h
@@ -1,6 +1,7 @@
 #ifndef BASEINTERFACE_H
 #define BASEINTERFACE_H
 
+#include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
@@ -15,11 +16,27 @@ protected:
     vector<int> gg;
     vector<size_t> ig;
     vector<size_t> jg;
+
+    //For Unit-test
+    size_t size_answer;
+    vector<int> gg_answer;
+    vector<size_t> ig_answer;
+    vector<size_t> jg_answer;
+
+    bool read_from_file_service(string file_name, bool is_unit_test);
+    bool read_answer_from_file(string file_name);
+    size_t compare();
+    size_t test1();
+    size_t test2();
+    size_t test3();
 public:
 
     bool read_from_file(string file_name);
     bool write_to_file(string file_name);
     virtual void solve() = 0;
+
+    //For Unit-test
+    size_t run_tests();
 };
 
 #endif // BASEINTERFACE_H
